758ed978d4f93f2826feae0e23ba6733bd2b16cf
szymonrybczak <szymon.rybczak@gmail.com>
2024-07-06 13:17:29 +0200

    fix: update app source

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page({
             </a>
         </p>
         <Link
-          href="https://github.com/szymonrybczak"
+          href="https://github.com/szymonrybczak/isappusingreactnative.com"
           className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           prefetch={false}
         >
