5e09ac431045b8c4f061188c60c05c1975225ec0
szymonrybczak <szymon.rybczak@gmail.com>
2024-07-06 22:56:22 +0200

    fix: bump `maxDuration` for Server Actions

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { List } from "@/components/List";
 import { Search } from "@/components/search";
 import Link from "next/link";
 
-export const maxDuration = 60;
+export const maxDuration = 120;
 
 export default function Page({
   searchParams,
