fd25969afae08ad53de574a7f2bafd3bbab526d5
szymonrybczak <szymon.rybczak@gmail.com>
2024-07-14 12:06:14 +0200

    fix: warnings in svg

--- components/icons/React.tsx
+++ components/icons/React.tsx
@@ -12,11 +12,11 @@ export default function ReactLogo(props: React.SVGProps<SVGSVGElement>) {
       <g
         id="Page-1"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       >
-        <g id="React-Logo-Filled-(1)" fill="#087EA4" fill-rule="nonzero">
+        <g id="React-Logo-Filled-(1)" fill="#087EA4" fillRule="nonzero">
           <path
             d="M285.5,201 C255.400481,201 231,225.400481 231,255.5 C231,285.599519 255.400481,310 285.5,310 C315.599519,310 340,285.599519 340,255.5 C340,225.400481 315.599519,201 285.5,201"
             id="Path"
